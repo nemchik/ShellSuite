d7b7888ffe4a09e2fd48e7ed98fe3affaa3fab01
Eric Nemchik <eric@nemchik.com>
2018-11-29 00:09:08 -0600

    ShellSuite

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# upgraded-waffle
+# ShellSuite
+
+[![Travis (.com) branch](https://img.shields.io/travis/com/nemchik/ShellSuite/master.svg?logo=travis)](https://travis-ci.com/nemchik/ShellSuite) [![GitHub](https://img.shields.io/github/license/nemchik/ShellSuite.svg)](https://github.com/nemchik/ShellSuite/blob/master/LICENSE.md)
+
+This is a collection of shell script validations designed to run on git repositories. The intention of this project is to be run by CI/CD such as [Travis CI](https://travis-ci.com/) and others, but you can also run it locally/manually.
+
+## Purpose
+
+These tools are already available for direct use in many CI/CD environments and otherwise, but after using them I've found some inconsistencies. This project aims to solve those inconsistencies by:
+
+- Using the same tool/version on each environment used for testing.
+- Using the latest available version of each tool via Docker.
+- Only scanning files that are part of the git repo using `git ls-tree` and skipping files ignored with `.gitignore`.
+
+## Script Arguments
+
+The following arguments are required:
+
+`-p` or `--path` must be defined first and should be the full path to the git repo being tested.
+
+`-v` or `--validator` accepts `bashate`, `shellcheck`, or `shfmt`.
+
+`-f` or `--flags` must start with a blank space and will be passed to the validator for testing. This allows you to ignore certain validation rules if needed.
+
+## Example useage
+
+All examples show the use of all 3 validators, but you may choose to use only one or two if you'd like.
+
+### Use in `.travis.yml`
+
+Examples are shown with recommended defaults for `--flags`, but you may adjust to your liking.
+
+Running from this repo:
+
+```yaml
+script:
+  - curl -fsSL https://raw.githubusercontent.com/nemchik/ShellSuite/master/shellsuite.sh -o shellsuite.sh && bash shellsuite.sh -p "${PWD}" -v "bashate" -f " -i E006"
+  - curl -fsSL https://raw.githubusercontent.com/nemchik/ShellSuite/master/shellsuite.sh -o shellsuite.sh && bash shellsuite.sh -p "${PWD}" -v "shellcheck" -f " -x"
+  - curl -fsSL https://raw.githubusercontent.com/nemchik/ShellSuite/master/shellsuite.sh -o shellsuite.sh && bash shellsuite.sh -p "${PWD}" -v "shfmt" -f " -s -i 4 -ci -sr -d"
+```
+
+Running from your own repo:
+
+This example assumes you have added `shellsuite.sh` to the root of your repo. If you add `shellsuite.sh` to a subfolder in your repo you will need to adjust `-p "${PWD}"` to `-p "${TRAVIS_BUILD_DIR}"` or `-p "/full/repo/path"`.
+
+```yaml
+script:
+  - bash shellsuite.sh -p "${PWD}" -v "bashate" -f " -i E006"
+  - bash shellsuite.sh -p "${PWD}" -v "shellcheck" -f " -x"
+  - bash shellsuite.sh -p "${PWD}" -v "shfmt" -f " -s -i 4 -ci -sr -d"
+```
+
+### Use locally/manually
+
+Requires Docker
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/nemchik/ShellSuite/master/shellsuite.sh -o shellsuite.sh
+bash shellsuite.sh -p "/full/repo/path" -v "bashate" -f " -i E006"
+bash shellsuite.sh -p "/full/repo/path" -v "shellcheck" -f " -x"
+bash shellsuite.sh -p "/full/repo/path" -v "shfmt" -f " -s -i 4 -ci -sr -d"
+```
+
+## Special Thanks
+
+- [christronyxyocum](https://github.com/christronyxyocum/) helping drive the motivation for this project's creation.
+- [caarlos0](https://github.com/caarlos0/) for the amazing [caarlos0/shell-ci-build](https://github.com/caarlos0/shell-ci-build) repo on which the primary function of this project is based.
---
+++ shellsuite.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Usage Information
+#/ Usage: shellsuite.sh [OPTION]
+#/
+#/ This is the main ShellSuite script.
+#/
+#/  -p --path
+#/  -v --validator
+#/  -f --flags
+#/
+usage() {
+    grep '^#/' "${SCRIPTNAME}" | cut -c4- || echo "Failed to display usage information."
+    exit
+}
+
+# Command Line Arguments
+readonly ARGS=("$@")
+
+# Script Information
+# https://stackoverflow.com/a/246128/1384186
+get_scriptname() {
+    local SOURCE
+    local DIR
+    SOURCE="${BASH_SOURCE[0]}"
+    while [[ -L ${SOURCE} ]]; do # resolve ${SOURCE} until the file is no longer a symlink
+        DIR="$(cd -P "$(dirname "${SOURCE}")" > /dev/null && pwd)"
+        SOURCE="$(readlink "${SOURCE}")"
+        [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}" # if ${SOURCE} was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    echo "${SOURCE}"
+}
+readonly SCRIPTNAME="$(get_scriptname)"
+readonly SCRIPTPATH="$(cd -P "$(dirname "${SCRIPTNAME}")" > /dev/null && pwd)"
+
+# User/Group Information
+readonly DETECTED_PUID=${SUDO_UID:-$UID}
+readonly DETECTED_UNAME=$(id -un "${DETECTED_PUID}" 2> /dev/null || true)
+readonly DETECTED_PGID=$(id -g "${DETECTED_PUID}" 2> /dev/null || true)
+readonly DETECTED_UGROUP=$(id -gn "${DETECTED_PUID}" 2> /dev/null || true)
+readonly DETECTED_HOMEDIR=$(eval echo "~${DETECTED_UNAME}" 2> /dev/null || true)
+
+# Colors
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+readonly BLU='\e[34m'
+readonly GRN='\e[32m'
+readonly RED='\e[31m'
+readonly YLW='\e[33m'
+readonly NC='\e[0m'
+
+# Log Functions
+readonly LOG_FILE="/tmp/shellsuite.log"
+sudo chown "${DETECTED_PUID:-$DETECTED_UNAME}":"${DETECTED_PGID:-$DETECTED_UGROUP}" "${LOG_FILE}" > /dev/null 2>&1 || true # This line should always use sudo
+info() { echo -e "$(date +"%F %T") ${BLU}[INFO]${NC}       $*" | tee -a "${LOG_FILE}" >&2; }
+warning() { echo -e "$(date +"%F %T") ${YLW}[WARNING]${NC}    $*" | tee -a "${LOG_FILE}" >&2; }
+error() { echo -e "$(date +"%F %T") ${RED}[ERROR]${NC}      $*" | tee -a "${LOG_FILE}" >&2; }
+fatal() {
+    echo -e "$(date +"%F %T") ${RED}[FATAL]${NC}      $*" | tee -a "${LOG_FILE}" >&2
+    exit 1
+}
+
+cmdline() {
+    # http://www.kfirlavi.com/blog/2012/11/14/defensive-bash-programming/
+    # http://kirk.webfinish.com/2009/10/bash-shell-script-to-use-getopts-with-gnu-style-long-positional-parameters/
+    local ARG=
+    local LOCAL_ARGS
+    for ARG; do
+        local DELIM=""
+        case "${ARG}" in
+            #translate --gnu-long-options to -g (short options)
+            --flags) LOCAL_ARGS="${LOCAL_ARGS:-}-f " ;;
+            --path) LOCAL_ARGS="${LOCAL_ARGS:-}-p " ;;
+            --validator) LOCAL_ARGS="${LOCAL_ARGS:-}-v " ;;
+            --debug) LOCAL_ARGS="${LOCAL_ARGS:-}-x " ;;
+            #pass through anything else
+            *)
+                [[ ${ARG:0:1} == "-" ]] || DELIM='"'
+                LOCAL_ARGS="${LOCAL_ARGS:-}${DELIM}${ARG}${DELIM} "
+                ;;
+        esac
+    done
+
+    #Reset the positional parameters to the short options
+    eval set -- "${LOCAL_ARGS:-}"
+
+    while getopts ":f:p:v:x" OPTION; do
+        case ${OPTION} in
+            f)
+                if [[ ${OPTARG:0:1} != " " ]]; then
+                    fatal "Flags must start with a space."
+                fi
+                readonly VALIDATIONFLAGS="${OPTARG[*]}"
+                ;;
+            p)
+                readonly VALIDATIONPATH="${OPTARG[*]}"
+                ;;
+            v)
+                if [[ -z ${VALIDATIONPATH:-} ]]; then
+                    fatal "Path must be defined first."
+                fi
+                case ${OPTARG} in
+                    bashate)
+                        readonly VALIDATOR="docker run --rm -v ${VALIDATIONPATH}:${VALIDATIONPATH} textclean/bashate"
+                        readonly VALIDATORCHECK="--show"
+                        ;;
+                    shellcheck)
+                        readonly VALIDATOR="docker run --rm -v ${VALIDATIONPATH}:${VALIDATIONPATH} koalaman/shellcheck"
+                        readonly VALIDATORCHECK="--version"
+                        ;;
+                    shfmt)
+                        #readonly VALIDATOR="docker run --rm -v ${VALIDATIONPATH}:${VALIDATIONPATH} jamesmstone/shfmt"
+                        readonly VALIDATOR="docker run --rm -v ${VALIDATIONPATH}:${VALIDATIONPATH} mvdan/shfmt:v2.6.1 shfmt"
+                        readonly VALIDATORCHECK="--version"
+                        ;;
+                    *)
+                        fatal "Invalid validator option."
+                        ;;
+                esac
+                ;;
+            x)
+                readonly DEBUG='-x'
+                set -x
+                ;;
+            :)
+                fatal "${OPTARG} requires an option."
+                ;;
+            *)
+                usage
+                exit
+                ;;
+        esac
+    done
+    return 0
+}
+
+# Main Function
+main() {
+    # Arch Check
+    readonly ARCH=$(uname -m)
+    if [[ ${ARCH} != "x86_64" ]]; then
+        fatal "Unsupported architecture."
+    fi
+
+    # Set command line variables
+    cmdline "${ARGS[@]:-}"
+
+    # Confirm variables are set
+    if [[ -z ${VALIDATIONPATH:-} ]]; then
+        fatal "Path must be defined."
+    fi
+    if [[ -z ${VALIDATOR:-} ]]; then
+        fatal "Validator must be defined."
+    fi
+    if [[ -z ${VALIDATIONFLAGS:-} ]]; then
+        fatal "Flags must be defined."
+    fi
+    if [[ -z ${VALIDATORCHECK:-} ]]; then
+        fatal "Check must be defined."
+    fi
+
+    # Check that the validator is installed
+    eval "${VALIDATOR} ${VALIDATORCHECK}" || fatal "Failed to check ${VALIDATOR} version."
+
+    # https://github.com/caarlos0/shell-ci-build
+    info "Linting all executables and .*sh files with ${VALIDATOR}..."
+    while IFS= read -r line; do
+        if head -n1 "${VALIDATIONPATH}/${line}" | grep -q -E -w "sh|bash|dash|ksh"; then
+            eval "${VALIDATOR} ${VALIDATIONFLAGS[*]} ${VALIDATIONPATH}/${line}" || fatal "Linting ${line}"
+            info "Linting ${line}"
+        else
+            warning "Skipping ${line}..."
+        fi
+    done < <(git -C "${VALIDATIONPATH}" ls-tree -r HEAD | grep -E '^1007|.*\..*sh$' | awk '{print $4}')
+    info "${VALIDATOR} validation complete."
+}
+main
