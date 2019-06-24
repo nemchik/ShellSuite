7ffbce978f415567b6b3869fdf7b570520678ff4
Eric Nemchik <eric@nemchik.com>
2019-06-24 09:34:02 -0500

    Cleanup use of quotes

--- shellsuite.sh
+++ shellsuite.sh
@@ -13,7 +13,7 @@ IFS=$'\n\t'
 #/  -t --tag (optional)
 #/
 usage() {
-    grep '^#/' "${SCRIPTNAME}" | cut -c4- || echo "Failed to display usage information."
+    grep --color=never -Po '^#/\K.*' "${SCRIPTNAME}" || echo "Failed to display usage information."
     exit
 }
 
@@ -23,16 +23,17 @@ readonly ARGS=("$@")
 # Script Information
 # https://stackoverflow.com/questions/59895/get-the-source-directory-of-a-bash-script-from-within-the-script-itself/246128#246128
 get_scriptname() {
-    local SOURCE="${BASH_SOURCE[0]:-$0}" # https://stackoverflow.com/questions/35006457/choosing-between-0-and-bash-source/35006505#35006505
+    # https://stackoverflow.com/questions/35006457/choosing-between-0-and-bash-source/35006505#35006505
+    local SOURCE=${BASH_SOURCE[0]:-$0}
     while [[ -L ${SOURCE} ]]; do # resolve ${SOURCE} until the file is no longer a symlink
         local DIR
-        DIR="$(cd -P "$(dirname "${SOURCE}")" > /dev/null 2>&1 && pwd)"
-        SOURCE="$(readlink "${SOURCE}")"
+        DIR=$(cd -P "$(dirname "${SOURCE}")" > /dev/null 2>&1 && pwd)
+        SOURCE=$(readlink "${SOURCE}")
         [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}" # if ${SOURCE} was a relative symlink, we need to resolve it relative to the path where the symlink file was located
     done
     echo "${SOURCE}"
 }
-readonly SCRIPTPATH="$(cd -P "$(dirname "$(get_scriptname)")" > /dev/null 2>&1 && pwd)"
+readonly SCRIPTPATH=$(cd -P "$(dirname "$(get_scriptname)")" > /dev/null 2>&1 && pwd)
 readonly SCRIPTNAME="${SCRIPTPATH}/$(basename "$(get_scriptname)")"
 
 # User/Group Information
@@ -43,27 +44,27 @@ readonly DETECTED_UGROUP=$(id -gn "${DETECTED_PUID}" 2> /dev/null || true)
 readonly DETECTED_HOMEDIR=$(eval echo "~${DETECTED_UNAME}" 2> /dev/null || true)
 
 # Terminal Colors
-if [[ -t 1 ]] || [[ ${CI:-} == true ]]; then
+if [[ ${CI:-} == true ]] || [[ -t 1 ]]; then
     # Reference for colornumbers used by most terminals can be found here: https://jonasjacek.github.io/colors/
     # The actual color depends on the color scheme set by the current terminal-emulator
     # For capabilities, see terminfo(5)
     if [[ $(tput colors) -ge 8 ]]; then
-        BLU="$(tput setaf 4)"
-        GRN="$(tput setaf 2)"
-        RED="$(tput setaf 1)"
-        YLW="$(tput setaf 3)"
-        NC="$(tput sgr0)"
+        BLU=$(tput setaf 4)
+        GRN=$(tput setaf 2)
+        RED=$(tput setaf 1)
+        YLW=$(tput setaf 3)
+        NC=$(tput sgr0)
     fi
 fi
-readonly BLU="${BLU:-}"
-readonly GRN="${GRN:-}"
-readonly RED="${RED:-}"
-readonly YLW="${YLW:-}"
-readonly NC="${NC:-}"
+readonly BLU=${BLU:-}
+readonly GRN=${GRN:-}
+readonly RED=${RED:-}
+readonly YLW=${YLW:-}
+readonly NC=${NC:-}
 
 # Log Functions
 readonly LOG_FILE="/tmp/shellsuite.log"
-sudo chown "${DETECTED_PUID:-$DETECTED_UNAME}":"${DETECTED_PGID:-$DETECTED_UGROUP}" "${LOG_FILE}" > /dev/null 2>&1 || true # This line should always use sudo
+sudo chown "${DETECTED_PUID:-$DETECTED_UNAME}":"${DETECTED_PGID:-$DETECTED_UGROUP}" "${LOG_FILE}" > /dev/null 2>&1 || true
 info() { echo -e "${NC}$(date +"%F %T") ${BLU}[INFO]${NC}       $*${NC}" | tee -a "${LOG_FILE}" >&2; }
 warning() { echo -e "${NC}$(date +"%F %T") ${YLW}[WARNING]${NC}    $*${NC}" | tee -a "${LOG_FILE}" >&2; }
 error() { echo -e "${NC}$(date +"%F %T") ${RED}[ERROR]${NC}      $*${NC}" | tee -a "${LOG_FILE}" >&2; }
@@ -104,19 +105,19 @@ cmdline() {
                 if [[ ${OPTARG:0:1} != " " ]]; then
                     fatal "Flags must start with a space."
                 fi
-                readonly VALIDATIONFLAGS="${OPTARG[*]}"
+                readonly VALIDATIONFLAGS=${OPTARG[*]}
                 ;;
             p)
-                readonly VALIDATIONPATH="${OPTARG[*]}"
+                readonly VALIDATIONPATH=${OPTARG[*]}
                 ;;
             t)
-                readonly VALIDATIONTAG="${OPTARG[*]}"
+                readonly VALIDATIONTAG=${OPTARG[*]}
                 ;;
             v)
                 if [[ -z ${VALIDATIONPATH:-} ]]; then
                     fatal "Path must be defined first."
                 fi
-                readonly VALIDATOR="${OPTARG}"
+                readonly VALIDATOR=${OPTARG}
                 case ${VALIDATOR} in
                     bashate)
                         readonly VALIDATIONCMD="docker run --rm -v ${VALIDATIONPATH}:${VALIDATIONPATH} textclean/bashate"
